c67ac4ff65df40c625729761a0454919582c8eb0
AlexKobilianskiy <alexey.kobilianskiy@gmail.com>
2021-11-09 14:17:48 +0200

    users table adaptation

--- src/components/UsersTable/UsersTable.jsx
+++ src/components/UsersTable/UsersTable.jsx
@@ -10,11 +10,20 @@ import { Box,
   TablePagination } from '@mui/material';
 import UsersTableHead from './UsersTableHead';
 import UserBadge from '../UserBadge/UserBadge';
+import {makeStyles} from '@mui/styles';
+
+const useStyles = makeStyles({
+  tableCell: {
+    paddingTop: 6,
+    paddingBottom: 7
+  }
+})
 
 const UsersTable = ({users, onPickUser}) => {
+  const classes = useStyles();
   const [selected, setSelected] = useState([]);
   const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
 
   const handleSelectAllClick = e => {
     if (e.target.checked) {
@@ -61,82 +70,88 @@ const UsersTable = ({users, onPickUser}) => {
   const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - users.length) : 0;
 
   return (
-    <Box sx={{ width: '100%' }}>
-      <Paper sx={{ width: '100%', mb: 2 }}>
-        <TableContainer>
-          <Table
-            sx={{ minWidth: 750 }}
-            size={'medium'}
-          >
-            <UsersTableHead
-              numSelected={selected.length}
-              onSelectAllClick={handleSelectAllClick}
-              rowCount={users.length}
-            />
-            <TableBody>
-              {users
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row, index) => {
-                  const isItemSelected = isSelected(row.name);
-                  const labelId = `enhanced-table-checkbox-${index}`;
+    <>
+      <TableContainer component={Paper} className="table-container">
+        <Table
+          sx={{ minWidth: 800 }}
+          size={'medium'}
+        >
+          <UsersTableHead
+            numSelected={selected.length}
+            onSelectAllClick={handleSelectAllClick}
+            rowCount={users.length}
+          />
+          <TableBody>
+            {users
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row, index) => {
+                const isItemSelected = isSelected(row.name);
+                const labelId = `enhanced-table-checkbox-${index}`;
 
-                  return (
-                    <TableRow
-                      hover
-                      onClick={(e) => onPickUser(row)}
-                      role="checkbox"
-                      aria-checked={isItemSelected}
-                      tabIndex={-1}
-                      key={row.name}
-                      selected={isItemSelected}
+                return (
+                  <TableRow
+                    hover
+                    onClick={(e) => onPickUser(row)}
+                    role="checkbox"
+                    aria-checked={isItemSelected}
+                    tabIndex={-1}
+                    key={row.name}
+                    selected={isItemSelected}
+                  >
+                    <TableCell padding="checkbox" className={classes.tableCell}>
+                      <Checkbox
+                        color="primary"
+                        checked={isItemSelected}
+                        inputProps={{
+                          'aria-labelledby': labelId,
+                        }}
+                        onClick={(event) => handleClick(event, row.name)}
+                      />
+                    </TableCell>
+                    <TableCell
+                      component="th"
+                      id={labelId}
+                      scope="row"
+                      className={classes.tableCell}
                     >
-                      <TableCell padding="checkbox">
-                        <Checkbox
-                          color="primary"
-                          checked={isItemSelected}
-                          inputProps={{
-                            'aria-labelledby': labelId,
-                          }}
-                          onClick={(event) => handleClick(event, row.name)}
-                        />
-                      </TableCell>
-                      <TableCell
-                        component="th"
-                        id={labelId}
-                        scope="row"
-                      >
-                        <UserBadge name={row.name} />
-                      </TableCell>
-                      <TableCell>{row.id}</TableCell>
-                      <TableCell>{row.phone}</TableCell>
-                      <TableCell>{row.email}</TableCell>
-                      <TableCell align="right">11/08/2021</TableCell>
-                    </TableRow>
-                  );
-                })}
-              {emptyRows > 0 && (
-                <TableRow
-                  style={{
-                    height: 53 * emptyRows,
-                  }}
-                >
-                  <TableCell colSpan={6} />
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[5, 10, 25]}
-          component="div"
-          count={users.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-      </Paper>
-    </Box>
+                      <UserBadge name={row.name} />
+                    </TableCell>
+                    <TableCell className={classes.tableCell}>{row.id}</TableCell>
+                    <TableCell className={classes.tableCell}>{row.phone}</TableCell>
+                    <TableCell className={classes.tableCell}>{row.email}</TableCell>
+                    <TableCell
+                      align="right"
+                      style={{color: '#9EA0A5'}}
+                      className={classes.tableCell}
+                    >
+                      11/08/2021
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            {emptyRows > 0 && (
+              <TableRow
+                style={{
+                  height: 53 * emptyRows,
+                }}
+              >
+                <TableCell colSpan={6} />
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={users.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </>
+
   );
 };
 
